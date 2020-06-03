3627fa766e8363c6c82dd15eae4cea23163b336c
Peter Sankiewicz <petsan@hotmail.com>
2020-06-03 12:07:55 -0700

    values-vs-reference

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -76,7 +76,9 @@ const addBirthYearProperty = (obj, year) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5 - Stretch Goal
 
-Write a function that accepts an array of people objects and adds a new property called isAuthor to each object in the list. Set the value of the new property to true.
+Write a function that accepts an array of people objects and
+adds a new property called isAuthor to each object in the list.
+Set the value of the new property to true.
 
 The function should modify the object in place. Do not use a return statement.
 
@@ -108,6 +110,7 @@ console.log(a) prints [1, 2, 3, 4]
 const append = (arr1, arr2) => {
   // Solution code here...
 
+  arr1.push.apply(arr1, arr2);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -166,7 +169,7 @@ xdescribe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
